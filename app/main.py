6d589be63e317aeb963061a70a515f6a63516b2a
berkerbasergun <bberkerbasergun@gmail.com>
2026-05-12 00:14:46 +0200

    add comment in main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from app.core.config import settings
 from app.api.routes import router
@@ -5,7 +6,7 @@ from app.api.routes import router
 app =FastAPI(title=settings.app_name)
 
 app.include_router(router)
-
+# karsilama ekrani
 @app.get("/")
 async def root():
     return {
